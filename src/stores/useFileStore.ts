850d95f3ac27e34604d3173e1dab67b272fe05ad
GALBICCIM <godvictory0909@gmail.com>
2025-09-25 17:19:36 +0900

    feat: Implement uploaded file array

--- src/stores/useFileStore.ts
+++ src/stores/useFileStore.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
 
 interface FileStore {
-	file: File | null;
-	setFile: (file: File | null) => void;
+	selectedFile: File | null;
+	files: File[];
+	setFile: (selectedFile: File | null) => void;
+	addFile: (file: File | null) => void;
 }
 
 export const useFileStore = create<FileStore>((set) => ({
-	file: null,
-	setFile: (file) => set({ file }),
+	selectedFile: null,
+	files: [],
+	setFile: (selectedFile) => set({ selectedFile }),
+	addFile: (file) => {
+		if (file !== null) {
+			set((state) => ({ files: [file, ...state.files] }));
+		}
+	},
 }));
