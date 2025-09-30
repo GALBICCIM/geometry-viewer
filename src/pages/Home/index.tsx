d13e266729ddeb83ac88745f3478a8fed8d621f1
GALBICCIM <godvictory0909@gmail.com>
2025-09-30 11:33:41 +0900

    feat: Add sample geometries & Initialize sample geometries

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFileStore } from "@/stores";
 import { sceneRouter } from "@/loaders";
 
+import { SAMPLE_FILES } from "@/constants";
+
 import * as S from "./styled";
 
 const Home = () => {
+	const [isLoading, setIsLoading] = useState(true);
 	const navigator = useNavigate();
 	const selectedFile = useFileStore((state) => state.selectedFile);
 	const files = useFileStore((state) => state.files);
@@ -29,24 +33,55 @@ const Home = () => {
 		}
 	};
 
+	useEffect(() => {
+		(async () => {
+			const dt = new DataTransfer();
+
+			try {
+				for (const fname of SAMPLE_FILES) {
+					const res = await fetch(`/sample_geometries/${fname}`);
+					const blob = await res.blob();
+					const file = new File([blob], fname);
+
+					dt.items.add(file);
+				}
+
+				addFile(dt.files);
+			} catch (e) {
+				console.error(e);
+			} finally {
+				setIsLoading(false);
+			}
+		})();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<>
 			<S.Container>
-				<S.FileSelectMenu>
-					<S.Wrapper width="100%" justify="space-around" items="center">
-						<S.Text color="black">Geometries ({files.length})</S.Text>
-						<S.FileInputLabel htmlFor="input-file">
-							<span style={{ transform: "translateY(5px)" }}>+</span>
-						</S.FileInputLabel>
-					</S.Wrapper>
-					{files &&
-						files.map((file, index) => (
-							<S.FileSelectButton key={index} isSelected={selectedFile === file} onClick={() => handleSelectGeometry(index)}>
-								{file.name}
-							</S.FileSelectButton>
-						))}
-				</S.FileSelectMenu>
-				<S.GoViewer onClick={handleButtonClick}>Go!</S.GoViewer>
+				{isLoading ? (
+					<S.Text color="white">Loading geometries...</S.Text>
+				) : (
+					<>
+						<S.FileSelectMenu>
+							<S.Wrapper width="45%" justify="space-around" items="center">
+								<S.Text color="black">Geometries ({files.length})</S.Text>
+								<S.FileInputLabel htmlFor="input-file">
+									<span style={{ transform: "translateY(5px)" }}>+</span>
+								</S.FileInputLabel>
+							</S.Wrapper>
+							<S.ItemWrapper>
+								{files &&
+									files.map((file, index) => (
+										<S.FileSelectButton key={index} isSelected={selectedFile === file} onClick={() => handleSelectGeometry(index)}>
+											{file.name}
+										</S.FileSelectButton>
+									))}
+							</S.ItemWrapper>
+						</S.FileSelectMenu>
+						<S.GoViewer onClick={handleButtonClick}>Go!</S.GoViewer>
+					</>
+				)}
 			</S.Container>
 			<S.FileInput type="file" id="input-file" accept=".vtp,.h5" multiple onChange={handleFileUpload} />
 		</>
