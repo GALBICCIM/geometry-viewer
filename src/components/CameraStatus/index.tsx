d78b6a447f7f363901620390610cc1f019dc4cd8
GALBICCIM <godvictory0909@gmail.com>
2025-09-30 16:51:00 +0900

    refactor: Code refactoring

--- src/components/CameraStatus/index.tsx
+++ src/components/CameraStatus/index.tsx
@@ -1,24 +1,16 @@
-import type { Vector3 } from "@babylonjs/core";
+import type { CameraStatusProps } from "@/types";
 
 import * as S from "./styled";
 
-interface StatusProps {
-	name: string;
-	alpha: string;
-	beta: string;
-	radius: string;
-	target: Vector3;
-}
-
-const CameraStatus = ({ name, alpha, beta, radius, target }: StatusProps) => (
+const CameraStatus = ({ name, alpha, beta, radius, target }: CameraStatusProps) => (
 	<S.Container>
 		<S.Text size={1.5}>geometry: {name.replace(/\..*$/, "")}</S.Text>
 		<S.Wrapper>
-			<S.Text size={1}>rotation-x: {alpha}</S.Text>
-			<S.Text size={1}>rotation-y: {beta}</S.Text>
-			<S.Text size={1}>distance: {radius}</S.Text>
+			<S.Text size={1}>rotation-x: {alpha.toFixed(2)}</S.Text>
+			<S.Text size={1}>rotation-y: {beta.toFixed(2)}</S.Text>
+			<S.Text size={1}>distance: {radius.toFixed(0)}</S.Text>
 			<S.Text size={1}>
-				target: | x: {target._x.toFixed(3)} | y: {target._y.toFixed(3)} | z: {target._z.toFixed(3)} |
+				target: | x: {target._x.toFixed(0)} | y: {target._y.toFixed(0)} | z: {target._z.toFixed(0)} |
 			</S.Text>
 		</S.Wrapper>
 	</S.Container>
